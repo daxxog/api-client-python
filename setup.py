2c33fefd08e90be514da61e15c30b633839a99aa
David Volm <david@volminator.com>
2021-07-24 12:54:40 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     ],
     install_requires=[
         "appdirs==1.4.4",
-        "certifi==2020.6.20",
+        "certifi",
         "chardet==3.0.4",
         "idna==2.10",
         "packaging==20.4",
